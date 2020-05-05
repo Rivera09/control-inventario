5e5e026e64ccafb1bf91a2b97718a39481840d24
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-05 17:09:30 -0600

    Agregado ddl para crear tabla productos

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mssql = require("mssql");
-// require('dotenv').config({ path: './confing.env' });
+require('dotenv').config({ path: './confing.env' });
 
 const db = async () => {
   try {
     // await mssql.connect(
     //   "URI"
     // );
-    await mssql.connect(process.env.DB_URI);
+    await mssql.connect("mssql://adminUser:asd.4567@20161002921.database.windows.net/inventario?encrypt=true");
     console.log("Remote DB connected");
   } catch (error) {
     console.error(error.message);
