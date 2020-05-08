f30c5e9249e29d546d9a37eb46a59c06dff25969
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-08 13:16:49 -0600

    Limpiando comentarios

--- config/db.js
+++ config/db.js
@@ -11,20 +11,3 @@ const conexion = new Sequelize(DB_NAME,DB_USER,DB_PASS,{
       }
 });
 module.exports = conexion;
-// const mssql = require("mssql");
-// require('dotenv').config({ path: './confing.env' });
-
-// const db = async () => {
-//   try {
-//     // await mssql.connect(
-//     //   "URI"
-//     // );
-//     await mssql.connect();
-//     console.log("Remote DB connected");
-//   } catch (error) {
-//     console.error(error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = db;
