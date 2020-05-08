b0b679fc107bbdddaa23e7fd103d0a7e99f6cfc0
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-07 20:25:23 -0600

    Configurando Sequelize

--- config/db.js
+++ config/db.js
@@ -1,17 +1,19 @@
-const mssql = require("mssql");
-require('dotenv').config({ path: './confing.env' });
+const Sequelize = require('sequelize');
 
-const db = async () => {
-  try {
-    // await mssql.connect(
-    //   "URI"
-    // );
-    await mssql.connect();
-    console.log("Remote DB connected");
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
-};
+// const mssql = require("mssql");
+// require('dotenv').config({ path: './confing.env' });
 
-module.exports = db;
+// const db = async () => {
+//   try {
+//     // await mssql.connect(
+//     //   "URI"
+//     // );
+//     await mssql.connect();
+//     console.log("Remote DB connected");
+//   } catch (error) {
+//     console.error(error.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const express = require('express');
-const db = require('./config/db');
+// const db = require('./config/db');
 
-db();
+
+//
+const { Sequelize, Model, DataTypes } = require('sequelize');
+// const Sequelize = require('sequelize');
+
+let conexion = new Sequelize('ingresos ','adminUser','asd.4567',{
+    host:'20161002921.database.windows.net',
+    dialect:'mssql',
+    dialectOptions: { 
+        options: {
+          encrypt: true,
+          schema:'example'
+        }
+      }
+});
+
+let Categories = conexion.define('Categories',{
+    CategoryName:Sequelize.STRING,
+    Description:Sequelize.STRING
+});
+
+// conexion.sync().then(()=>{
+//     Categories.create({
+//         CategoryName:"Test category",
+//         Description:"This is a conection test"
+//     })
+// },err=>{
+//     console.log(err);
+// })
+// 
+
+// db();
 
 const app = express();
 app.use(express.json({extended:false}));
