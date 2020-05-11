f813ab598965c7b34e07ac7bc7e7e3e0e6772f08
Marcela <marcelagissamador@gmail.com>
2020-05-11 15:08:09 -0600

    Creacion de Categorias en Controllers

---
+++ controllers/categorias.js
@@ -0,0 +1,15 @@
+const Categorias = require("../modules/Categorias");
+const repuestaError = require("../utils/respuestaError");
+exports.ObtenerCategorias= async (req, res) => {
+  try {
+    res.json(await Categorias.ObtnerCategorias());
+  } catch (e) {
+    console.log(e);
+    return repuestaError(
+      500,
+      "Error de servidor",
+      [{ msg: "Error al intentar obtener las categorias" }],
+      res
+    );
+  }
+};
