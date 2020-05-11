1f75d32f8989ef1fb707dc0e5b39dfaa545f1b47
EstefanyMoncada <estefanymassiel.31@gmail.com>
2020-05-11 01:18:37 -0600

    Revert " creado el modulo cliente, route cliente y controller cliente, modificacion de server.js"
    This reverts commit 83d60de547338a9ba760b10df2796f94f2bbc59d.

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ const auth = require('./routes/auth');
 app.use('/api/isv',isv);
 app.use('/api/usuarios',usuarios);
 app.use('/api/auth',auth);
-app.use('/api/clientes', clientes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Servidor corriendo en el pueto ${PORT}`));
