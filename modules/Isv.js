99178e6b8321eeb333f4255ffc9db95ce18cc731
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-11 22:25:39 -0600

    Creado enpoint para crear categoía

--- modules/Isv.js
+++ modules/Isv.js
@@ -1,18 +1,30 @@
-const { Sequelize } = require('sequelize');
-const conexion = require('../config/db');
-let Isv = conexion.define('Isv',{
-    porcentaje:Sequelize.FLOAT
-},{
-    timestamps:false,
-    schema:'Bodega'
-});
+const { Sequelize } = require("sequelize");
+const conexion = require("../config/db");
+let Isv = conexion.define(
+  "Isv",
+  {
+    porcentaje: Sequelize.FLOAT,
+  },
+  {
+    timestamps: false,
+    schema: "Bodega",
+  }
+);
 
-exports.crearIsv = async (porcentaje)=>{
-    try {
-        return await Isv.create({
-            porcentaje
-        })
-    } catch (e) {
-        throw new Error(e.message);
-    }
-}
+exports.crearIsv = async (porcentaje) => {
+  try {
+    return await Isv.create({
+      porcentaje,
+    });
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+exports.obtenerIsvPorId = async (id) => {
+  try {
+    return await Isv.findByPk(id);
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
--- routes/categorias.js
+++ routes/categorias.js
@@ -1,11 +1,24 @@
 const express = require("express");
-
-const auth = require('../middleware/auth');
-const acceso = require('../middleware/acceso');
-const { obtenerCategorias } = require("../controllers/categorias");
+const { check } = require("express-validator");
+const auth = require("../middleware/auth");
+const acceso = require("../middleware/acceso");
+const {
+  obtenerCategorias,
+  crearCategoria,
+} = require("../controllers/categorias");
 
 const router = express.Router();
 
-router.route("/").get(auth, acceso("Administrador","Gerente general"), obtenerCategorias);
+router
+  .route("/")
+  .get(auth, acceso("Administrador", "Gerente general"), obtenerCategorias);
+router
+  .route("/")
+  .post(
+    [check("descripcion").exists(), check("idIsv").isInt()],
+    auth,
+    acceso("Administrador", "Gerente general"),
+    crearCategoria
+  );
 
 module.exports = router;
