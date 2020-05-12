a07e53aab63f09d6eac7b6a8882b857524fb53c6
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-11 20:11:09 -0600

    -Implementado middlewares. -Identando el código. -Comentando unas funciones

--- routes/clientes.js
+++ routes/clientes.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const {crearCliente} = require("../controllers/clientes");
+const {check} = require('express-validator');
+const auth = require('../middleware/auth');
+const acceso = require('../middleware/acceso');
 
-router.route("/").post(crearCliente)
+
+router.route("/").post([
+    check("nombre").exists(),
+    check("email").isEmail(),
+    check("balance").isFloat(),
+    check("rtn").isLength({min:14,max:14})
+],auth,acceso("Administrador","Gerente general"),crearCliente)
 
 
 
