af9cc1738fa1344b73a4c0682739eb3bba3d8765
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-11 21:41:41 -0600

    Trabajando en el isv

--- controllers/proveedores.js
+++ controllers/proveedores.js
@@ -1,22 +1,27 @@
 const Proveedor = require("../modules/Proveedores");
+const { validationResult } = require("express-validator");
+const respuestaError = require("../utils/respuestaError");
 
-exports.crearProveedor = async(req, res)=>{
-    const{
-        nombre,
-        telefono,
-        email,
-    }=req.body;
 
-    try{
-        const proveedor = await Proveedor.crearProveedor(
-            nombre,
-            telefono,
-            email
-        );
-        return res.status(201).jason({Mesg:"Proveedor creado", id:proveedor.id, nombre:proveedor.nombre})
-    }
-    catch(e){
-        console.log(e)
-        return res.status(500).send("error de servidor")
-    }
-}
+//@route    POST api/proveedores/
+//@desc     Crear un nuevo proveedor.
+//@access   Private
+exports.crearProveedor = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return respuestaError(400, "Credenciales no válidas", errors.array(), res);
+
+  const { nombre, telefono, email } = req.body;
+
+  try {
+    const proveedor = await Proveedor.crearProveedor(nombre, telefono, email);
+    return res.status(201).json({
+      mensaje: "Proveedor creado",
+      id: proveedor.id,
+      nombre: proveedor.nombre,
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("error de servidor");
+  }
+};
