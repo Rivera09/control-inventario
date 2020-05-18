ed5d8766f6fe4ead23eaf66ec45c56bcc0c8b0da
Jorge Rivera <jorgeman201@yahoo.com>
2020-05-18 00:40:10 -0600

    -Creado landing page. -Creado login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Landing from "./components/layout/Landing";
+import Login from './components/login/Login';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
